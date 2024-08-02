e75bc9f34f81ec52830df5532dc5165bb16e445e
Kajal Kumari <156165044+HKajal@users.noreply.github.com>
2024-08-02 15:33:13 +0530

    Add files via upload

---
+++ scriptt.js
@@ -0,0 +1,10 @@
+function kajal(e){
+    let val=e.value;
+    var textarea=document.getElementById('txt');
+    textarea.style.fontSize=val+"px";
+}
+function bgclr(e){
+    let val=e.value;
+    var textarea=document.getElementById('bclor');
+    textarea.style.fontSize=val+"px";
+}
